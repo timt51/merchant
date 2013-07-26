fc98d9bcc27ead7bfebaf037827af55a0ebd2d0a
Timothy Truong, Jr. <timt51@live.com>
2013-07-26 14:30:55 -0400

    Iteration 3 Complete

---
+++ app/models/order_item.rb
@@ -0,0 +1,6 @@
+class OrderItem < ActiveRecord::Base
+	belongs_to :order
+	belongs_to :product
+	validates :order_id, presence: true
+	validates :product_id, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130726153115) do
+ActiveRecord::Schema.define(version: 20130726171849) do
+
+  create_table "order_items", force: true do |t|
+    t.integer  "product_id"
+    t.integer  "order_id"
+    t.integer  "quantity"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "orders", force: true do |t|
+    t.integer  "user_id"
+    t.string   "status"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", force: true do |t|
     t.string   "title"
