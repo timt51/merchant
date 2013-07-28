7d1f5a37f8980f0657b2b2074be6ce36adfa2b64
Timothy Truong, Jr. <timt51@live.com>
2013-07-28 18:30:47 -0400

    Fixed print_stock

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -6,7 +6,7 @@ module ProductsHelper
 	def print_stock(stock, requested = 1)
 		if stock == 0
 			content_tag(:span, "Out of Stock", class: "out_stock")
-		elsif stock > 0 && requested < stock
+		elsif stock > 0 && requested <= stock
 			content_tag(:span, "In Stock (#{stock})", class: "in_stock")
 		else
 			content_tag(:span, "Insufficient stock (#{stock})", class: "low_stock")
